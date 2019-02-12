b2e8e8e3556cb143fc47f1b4e2edf44afc07c50d
Shashank-Hiremath <shashankhiremath11@gmail.com>
2019-02-12 15:55:14 +0530

    Add kadanes algorithm, Reflections folder

---
+++ day-today codes/kadane.cpp
@@ -0,0 +1,25 @@
+//g++ kadane.cpp -o kadane;./kadane
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    int tma,ma,i,arr[]={-2,-3,4,-1,-2,1,5,-3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    for(i=0,ma=0,tma=0;i<n;i++)
+        {
+            tma+=arr[i];
+            tma=max(0,tma);
+            if(ma<tma)
+                ma=tma;
+            cout<<ma<<" ";
+        }
+    cout<<ma<<'\n';
+}
+
+
+
+
+
+
--- day-today codes/pascals-triangle.cpp
+++ day-today codes/pascals-triangle.cpp
@@ -1,12 +1,11 @@
+//g++ pascals-triangle.cpp -o pascals-triangle;./pascals-triangle
 #include <iostream>
 #include <cstring>
 
 using namespace std;
-
-int main()
+void pascal1(int n, int k)
 {
-    int n = 10, k = 5, i, j;
-    int dp[n + 1][k + 1];
+    int dp[n + 1][k + 1],i,j;
     memset(dp, 0, sizeof(dp));
     for (i = 0; i <= n; i++)
     {
@@ -17,4 +16,24 @@ int main()
                 dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
     }
     cout << "C(" << n << ", " << k << ") = " << dp[n][k] << '\n';
+}
+
+void pascal2(int n, int k)
+{
+    int c[k+1],i,j;
+    memset(c,0,sizeof(c));
+    c[0]=1;
+    for(i=1;i<=n;i++)
+        for(j=min(i,k);j>0;j--)
+            c[j]=c[j]+c[j-1];
+    cout<<c[k]<<'\n';
+}
+int main()
+{
+    int n = 10, k = 5;
+    
+    pascal1(n, k);
+    pascal2(n, k);
+    
+    return 0;
 }
